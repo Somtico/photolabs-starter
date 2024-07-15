4b3ac2e45676e4e6a54862623f3bdcba864ea3b6
Somto Ufondu <somtoufondu@gmail.com>
2024-07-15 13:43:09 -0600

    Change alt attributes to be more descriptive

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,17 +4,15 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = ({ photoData }) => {
   return (
     <div className="photo-list__item">
-      <div>
-        <img
-          src={photoData.imageSource}
-          alt="user image"
-          className="photo-list__image"
-        />
-      </div>
+      <img
+        src={photoData.imageSource}
+        alt="user's cover image"
+        className="photo-list__image"
+      />
       <div className="photo-list__user-details">
         <img
           src={photoData.profile}
-          alt="profile"
+          alt="user's profile photo"
           className="photo-list__user-profile"
         />
         <div className="photo-list__user-info">

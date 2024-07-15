f862b11afcf409cfa1dc4ed1c4078e14da2e7229
Somto Ufondu <somtoufondu@gmail.com>
2024-07-15 16:34:18 -0600

    Modify to consume props properly from the root application component, App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import HomeRoute from "routes/HomeRoute";
+import photos from "mocks/photos"; // Ensure this is the correct path
+import topics from "mocks/topics"; // Import your topics
 import "./App.scss";
 
 const App = () => {
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute topics={topics} photos={photos} />
     </div>
   );
 };

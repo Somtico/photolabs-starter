b1e13fce1fd01d343d5983a8b55a6c6196e0f2d4
Somto Ufondu <somtoufondu@gmail.com>
2024-07-15 15:52:25 -0600

    Add TopicList component to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // import PhotoListItem from "./components/PhotoListItem";
 import PhotoList from "./components/PhotoList";
+import TopicList from "components/TopicList";
 import "./App.scss";
 
 // const photos = new Array(3);
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <div className="App">
       {/* {photos.from(Array(3)).map((_, index) => <PhotoListItem key={index} photoData={sampleDataForPhotoListItem} />)} */}
+      <TopicList />
       <PhotoList />
     </div>
   );

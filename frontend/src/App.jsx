5f283d0fc5ce215f3033435aaa237686f9b82029
Somto Ufondu <somtoufondu@gmail.com>
2024-07-15 16:23:40 -0600

    Remove commented-out code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 
-// import PhotoListItem from "./components/PhotoListItem";
 import HomeRoute from "routes/HomeRoute";
 import "./App.scss";
-
-// const photos = new Array(3);
 
 const App = () => {
   return (
     <div className="App">
-      {/* {photos.from(Array(3)).map((_, index) => <PhotoListItem key={index} photoData={sampleDataForPhotoListItem} />)} */}
       <HomeRoute />
     </div>
   );

b580ae55f8e8a284066042a1b68bef44d8ac84aa
Somto Ufondu <somtoufondu@gmail.com>
2024-07-25 16:03:24 -0600

    Make changes to display the data fetched from the API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,24 @@
 import React, { useEffect, useCallback } from 'react';
 import HomeRoute from 'routes/HomeRoute';
-import photosMock from 'mocks/photos';
-import topicsMock from 'mocks/topics';
 import './App.scss';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import useApplicationData from 'hooks/useApplicationData';
 import useFavBadge from 'hooks/useFavBadge';
-import { ACTIONS } from 'context/actions';
 
 const App = () => {
   const {
-    state: { isModalVisible, selectedPhoto, photos, topics },
-    actions: { setSelectedPhoto, handleCloseModal, dispatch, setPhotos, setTopics },
+    state: { isModalVisible, selectedPhoto, photoData, topicData },
+    actions: { setSelectedPhoto, setIsModalVisible, handleCloseModal, dispatch },
   } = useApplicationData();
   
   const { selected, displayAlert, toggleFavourite } = useFavBadge();
 
-  const initializeData = useCallback(() => {
-    setPhotos(photosMock);
-    setTopics(topicsMock);
-  }, [setPhotos, setTopics]);
-
-  useEffect(() => {
-    initializeData();
-  }, [initializeData]);
-
   return (
     <div className="App">
       <HomeRoute
-        topics={topics}
-        photos={photos}
-        setIsModalVisible={() => dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS })}
+        topics={topicData}
+        photos={photoData}
+        setIsModalVisible={setIsModalVisible}
         setSelectedPhoto={setSelectedPhoto}
         favBadge={{ selected, displayAlert, toggleFavourite }}
       />
@@ -39,9 +27,9 @@ const App = () => {
           onClose={handleCloseModal}
           photo={selectedPhoto}
           favBadge={{ selected, displayAlert, toggleFavourite }}
-          photos={photos}
+          photos={photoData}
           setSelectedPhoto={setSelectedPhoto}
-          setIsModalVisible={() => dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS })}
+          setIsModalVisible={setIsModalVisible}
         />
       )}
     </div>

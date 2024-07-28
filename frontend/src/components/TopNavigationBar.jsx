ae417068526f9d55fae21bb22303da9a8f6299d4
Somto Ufondu <somtoufondu@gmail.com>
2024-07-27 20:18:34 -0600

    Format style

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,12 +3,23 @@ import TopicList from "./TopicList";
 import FavBadge from "./FavBadge";
 import "../styles/TopNavigationBar.scss";
 
-const TopNavigationBar = ({ topics, isFavPhotoExist, activeTopic, fetchPhotosByTopic }) => {
+const TopNavigationBar = ({
+  topics,
+  isFavPhotoExist,
+  activeTopic,
+  fetchPhotosByTopic,
+}) => {
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo"><a href="/">PhotoLabs</a></span>
+      <span className="top-nav-bar__logo">
+        <a href="/">PhotoLabs</a>
+      </span>
       <div className="top-nav-bar">
-        <TopicList topics={topics} activeTopic={activeTopic} fetchPhotosByTopic={fetchPhotosByTopic} />
+        <TopicList
+          topics={topics}
+          activeTopic={activeTopic}
+          fetchPhotosByTopic={fetchPhotosByTopic}
+        />
         <FavBadge isFavPhotoExist={isFavPhotoExist} />
       </div>
     </div>
--- frontend/src/hooks/useFavBadge.js
+++ frontend/src/hooks/useFavBadge.js
@@ -1,6 +1,6 @@
 import { useReducer } from "react";
 import { reducer } from "context/reducer";
-import { ACTIONS } from 'context/actions';
+import { ACTIONS } from "context/actions";
 
 const initialState = {
   selected: {},
@@ -11,7 +11,9 @@ const useFavBadge = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const toggleFavourite = (photoId) => {
-    const actionType = state.selected[photoId] ? ACTIONS.FAV_PHOTO_REMOVED : ACTIONS.FAV_PHOTO_ADDED;
+    const actionType = state.selected[photoId]
+      ? ACTIONS.FAV_PHOTO_REMOVED
+      : ACTIONS.FAV_PHOTO_ADDED;
     dispatch({ type: actionType, photoId });
     dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS }); // If displayAlert is to be managed here
   };
